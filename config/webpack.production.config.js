727fc8520fe82c2ab5917de81bf301f9ec39d7d4
Pema Geyleg <pema.geyleg@gmail.com>
2019-06-18 15:12:07 -0400

    Webpack support for css loader and mini-css-extract plugin

--- config/webpack.production.config.js
+++ config/webpack.production.config.js
@@ -1,12 +1,16 @@
 const path         = require('path');
 const Merge        = require('webpack-merge');
 const CommonConfig = require('./webpack.common.config.js');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = Merge(CommonConfig, {
   output: {
     path: path.join(__dirname, '../dist/production'),
     filename: 'bundle-[contenthash].js'
   },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: "styles-[contenthash].css" })
+  ],
   optimization: {
     minimize: true
   }
