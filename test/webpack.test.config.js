22a2e62f06484feda177c7bf29499d34bbcf57ad
Pema Geyleg <pema.geyleg@gmail.com>
2019-06-18 08:45:17 -0400

    Webpack manages test and jest understands ES6

---
+++ test/webpack.test.config.js
@@ -0,0 +1,36 @@
+# TODO:
+# We have to run webpack and then jest every time we change any file.
+
+const path = require('path');
+
+const glob = require('glob');
+
+const testFiles = glob.sync("**/*.test.js").
+  filter(function(element) {
+    return element != "test/bundle.test.js";
+  }).map(function(element) {
+    return "./" + element;
+  });
+
+module.exports = {
+  entry: testFiles,
+  output: {
+    path: path.resolve(__dirname, "."),
+    filename: "bundle.test.js"
+  },
+  mode: "none"
+};
+
+
+/* end new code */
+
+module.exports = {
+/* start new code */
+  entry: testFiles,
+/* end new code */
+  output: {
+    path: path.resolve(__dirname, "."),
+    filename: "bundle.test.js"
+  },
+  mode: "none"
+};
